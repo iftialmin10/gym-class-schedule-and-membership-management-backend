c5cca0f5026b34bef656e202433c108d63eaba29
Ifti-Alamin <iftialmin10@gmail.com>
2025-09-27 17:00:03 +0600

    initial setup done

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+export default app;
